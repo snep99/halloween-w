37c7f187ac1826ae09478faae481ba3d9a61287c
Yaroslav <25lizer25@gmail.com>
2022-10-25 17:18:09 +0200

    end <header> styles and menu js script

--- src/modules/show-menu.js
+++ src/modules/show-menu.js
@@ -19,3 +19,10 @@ if (navClose){
 }
 
 /* remove menu mobile */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
